ea1d26c5b7c70c8108cdd6e46a3cf1c2b5218af2
gio broliatto <giovanibroliatto@gmail.com>
2023-08-19 00:38:48 -0300

    refactor: global style from styled-components added

--- src/App.js
+++ src/App.js
@@ -5,10 +5,6 @@ const AppContainer = styled.div`
   width: 100vw;
   height: 100vh;
   background-image: linear-gradient(90deg, #32113b 35%, #6d5a70);
-
-  li {
-    list-style: none;
-  }
 `
 
 function App() {
