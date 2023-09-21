b05e151150eb99fa89ad01eed03296fbd776cd36
giobroliatto <giovanibroliatto@gmail.com>
2023-09-20 22:06:42 -0300

    feat: favorite section: add and remove added

---
+++ backend/services/favorites-services.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+function getAllFavorites() {
+    return JSON.parse(fs.readFileSync('favorites.json'));
+}
+
+function addFavorite(bookId) {
+  const books = JSON.parse(fs.readFileSync('books.json'));
+  const favorites = JSON.parse(fs.readFileSync('favorites.json'));
+
+  const addedBook = books.find(book => book.id === id);
+  const newBookList = [...favorites, addedBook];
+
+  fs.writeFileSync('favorites.json', JSON.stringify(newBookList));
+}
+
+function removeFavorite(id) {
+  const books = JSON.parse(fs.readFileSync('favorites.json'));
+
+  const filteredBooks = books.filter(book => book.id !== id);
+
+  fs.writeFileSync('favorites.json', JSON.stringify(filteredBooks));
+}
+
+module.exports = {
+  getAllFavorites,
+  addFavorite,
+  removeFavorite
+}
